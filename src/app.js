e001ffa9e7fd689c85b2325f1ba1802e791693c1
Dulce <dulce.samaroo@gmail.com>
2023-02-18 02:08:58 +0000

    inital commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+};
+
+let generateExcuse = () => {
+  let who = ["My kids ", "My husband ", "My friend ", "My students "];
+  let action = ["broke ", "hid ", "lost ", "took "];
+  let what = ["my laptop ", "the car ", "my glasses "];
+  let when = ["before class", "just now", "last night", "this morning"];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+  return (
+    who[whoIndex] + action[actionIndex] + what[whatIndex] + when[whenIndex]
+  );
 };
